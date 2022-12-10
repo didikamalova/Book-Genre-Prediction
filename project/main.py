10ae887d83eb0b38bcfd756a9519eba7a2d438a6
Ben Hoang <banhquanhoang@gmail.com>
2022-12-09 18:46:44 -0800

    cnn

--- project/main.py
+++ project/main.py
@@ -13,6 +13,8 @@ from evaluate import evaluate, get_labels
 
 
 if __name__ == "__main__":
+    train = False
+
     np.random.seed(229)
 
     transform = torchvision.transforms.Compose([
@@ -45,7 +47,7 @@ if __name__ == "__main__":
     throw_ratio = 2/3
     throwaway, train_set, val_set = \
         torch.utils.data.random_split(train_val_set, [(1-val_ratio)*throw_ratio, (1-val_ratio)*(1-throw_ratio), val_ratio],
-                                      generator=torch.Generator().manual_seed(229))
+                                    generator=torch.Generator().manual_seed(229))
 
     # TESTING DATASET
     test_data_dir = 'bookcover30-labels-test.csv'
@@ -54,9 +56,9 @@ if __name__ == "__main__":
     print(len(train_set), len(val_set), len(test_set))
 
     # HYPERPARAMS
-    batch_size = 64
+    batch_size = 32
     learning_rate = 0.001
-    num_epochs = 100
+    num_epochs = 10
     reg_lambda = 1e-6
 
     # AUGMENTATION
@@ -84,63 +86,65 @@ if __name__ == "__main__":
     # plt.show()
 
     # TRAINING
-    model = Model().to(device=device)
-    criterion = FocalLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    if train:
+        model = Model().to(device=device)
+        criterion = FocalLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+        # optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
-    start_time = time.time()
+        start_time = time.time()
 
-    best_model = model
-    highest = 0
+        best_model = model
+        highest = 0
 
-    print("=" * 50)
-    print("Initial Evaluation: ")
-    evaluate(model, train_loader, device, name="train")
-    evaluate(model, val_loader, device, name="val")
-    print("=" * 50)
-    for ix, epoch in enumerate(range(num_epochs), start=1):  # loop over the dataset multiple times
-        # TRAINING
-        print(f'EPOCH {ix}')
-        running_loss = 0.0
-        for i, (inputs, labels) in enumerate(tqdm(train_loader)):
-            # get the inputs; data is a list of [inputs, labels]
-            inputs = aug(inputs.to(device=device).to(dtype=torch.float32))
-            labels = labels.type(torch.LongTensor).to(device=device)
-
-            # zero the parameter gradients
-            optimizer.zero_grad(set_to_none=True)
-
-            # forward + backward + optimize
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
-            optimizer.step()
-
-            running_loss += loss.item()
-
-            # print statistics
-            """
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
-            """
-        train_accuracy = evaluate(model, train_loader, device, name="train")
-        train_loss = running_loss
-
-        # VALIDATION
-        with torch.no_grad():
-            val_accuracy = evaluate(model, val_loader, device, name="val")
-            if val_accuracy > highest:
-                highest = val_accuracy
-                best_model = model
         print("=" * 50)
+        print("Initial Evaluation: ")
+        evaluate(model, train_loader, device, name="train")
+        evaluate(model, val_loader, device, name="val")
+        print("=" * 50)
+        for ix, epoch in enumerate(range(num_epochs), start=1):  # loop over the dataset multiple times
+            # TRAINING
+            print(f'EPOCH {ix}')
+            running_loss = 0.0
+            for i, (inputs, labels) in enumerate(tqdm(train_loader)):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs = aug(inputs.to(device=device).to(dtype=torch.float32))
+                labels = labels.type(torch.LongTensor).to(device=device)
 
-    end_time = time.time()
-    print(f"Total training time: {end_time - start_time} sec")
+                # zero the parameter gradients
+                optimizer.zero_grad(set_to_none=True)
+
+                # forward + backward + optimize
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 10)
+                optimizer.step()
+
+                running_loss += loss.item()
+
+                # print statistics
+                """
+                if i % 2000 == 1999:    # print every 2000 mini-batches
+                    print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+                    running_loss = 0.0
+                """
+            train_accuracy = evaluate(model, train_loader, device, name="train")
+            train_loss = running_loss
+
+            # VALIDATION
+            with torch.no_grad():
+                val_accuracy = evaluate(model, val_loader, device, name="val")
+                if val_accuracy > highest:
+                    highest = val_accuracy
+                    best_model = model
+            print("=" * 50)
+
+        end_time = time.time()
+        print(f"Total training time: {end_time - start_time} sec")
 
     PATH = './book_covers.pth'
-    torch.save(model.state_dict(), PATH)
+    # torch.save(model.state_dict(), PATH)
 
     # PREDICT
     model = Model().to(device=device)
@@ -174,7 +178,7 @@ if __name__ == "__main__":
     df_cm = pd.DataFrame(cf_matrix/np.sum(cf_matrix) *10, index = [i for i in range(30)],
                         columns = [i for i in range(30)])
 
-    plt.figure(figsize = (12,7))
+    plt.figure(figsize = (24,14))
     sn.heatmap(df_cm, annot=True)
     plt.savefig('output.png')
 
